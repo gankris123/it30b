acfa606456636000a8f5a5592bd444cc139303bd
gankris123 <ganiemodesto300@gmail.com>
2026-08-12 19:32:20 +0800

    created borrow table

--- koy.sql
+++ koy.sql
@@ -6,3 +6,13 @@ CREATE TABLE student (
 );
 
 
+CREATE TABLE borrow (
+    Borrow_id INT AUTO_INCREMENT PRIMARY KEY,
+    Student_id INT NOT NULL,
+    Book_id INT NOT NULL,
+    Borrow_date DATE NOT NULL,
+    Return_date DATE,
+
+    FOREIGN KEY (Student_id) REFERENCES student(Student_id),
+    FOREIGN KEY (Book_id) REFERENCES book(Book_id)
+);

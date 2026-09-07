918b35812ee5636f804d93416bf42e75d75854f0
gankris123 <ganiemodesto300@gmail.com>
2026-09-08 07:52:25 +0800

    added table not exist student

---
+++ database/migrations/001_initial_library.sql
@@ -0,0 +1,19 @@
+-- #1 students table 
+CREATE TABLE IF NOT EXISTS students (
+  -- Primary key for the students table
+     student_id INT PRIMARY KEY AUTO_INCREMENT,
+
+        --Student name
+        student_first_name VARCHAR(50) NOT NULL,
+        student_last_name VARCHAR(50) NOT NULL,
+        
+        --student course
+        student_course VARCHAR(50) NOT NULL,
+       
+        --student created at timestamp
+        student_created_at TIMESTAMP NOT NULL
+        DEFAULT CURRENT_TIMESTAMP
+
+)ENGINE=InnoDB
+DEFAULT CHARSET=utf8mb4
+COLLATE=utf9mb4_general_ci;

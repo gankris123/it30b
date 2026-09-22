6f831439e38139b616393a239ea36b3bd24aca4d
gankris123 <remarromani@email.com>
2026-09-22 08:48:53 +0800

    added login interface

--- database/migrations/001_initial_library.sql
+++ database/migrations/001_initial_library.sql
@@ -1,71 +1,71 @@
--- #1 students table 
+-- #1 students table
 CREATE TABLE IF NOT EXISTS students (
-  -- Primary key for the students table
-     student_id INT PRIMARY KEY AUTO_INCREMENT,
-
-        --Student name
-        student_first_name VARCHAR(50) NOT NULL,
-        student_last_name VARCHAR(50) NOT NULL,
-        
-        --student course
-        student_course VARCHAR(50) NOT NULL,
-       
-        --student created at timestamp
-        student_created_at TIMESTAMP NOT NULL
+    -- Primary key for the students table
+    student_id INT AUTO_INCREMENT PRIMARY KEY,
+    
+    -- Student name
+    student_first_name VARCHAR(50) NOT NULL,
+    student_last_name VARCHAR(50) NOT NULL,
+    
+    -- Student course
+    student_course VARCHAR(50) NOT NULL,
+    
+    -- Student created at timestamp
+    student_created_at TIMESTAMP NOT NULL
         DEFAULT CURRENT_TIMESTAMP
-
-)ENGINE=InnoDB
+) ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
 COLLATE=utf8mb4_general_ci;
 
 -- #2 books table
 CREATE TABLE IF NOT EXISTS books (
-  -- Primary key for the books table
-     book_id INT PRIMARY KEY AUTO_INCREMENT,
+    -- Primary key for the books table
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
 
--- Book details
-book_title VARCHAR(100) NOT NULL,
-book_author VARCHAR(100) NOT NULL,
-book_category VARCHAR(50) NOT NULL,
--- Book created at timestamp
-book_created_at TIMESTAMP NOT NULL
-DEFAULT CURRENT_TIMESTAMP
+    -- Book details
+    book_title VARCHAR(100) NOT NULL,
+    book_author VARCHAR(100) NOT NULL,
+    book_category VARCHAR(50) NOT NULL,
 
-)ENGINE=InnoDB
+    -- Book created at timestamp
+    book_created_at TIMESTAMP NOT NULL
+        DEFAULT CURRENT_TIMESTAMP
+) ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_general_ci;  
+COLLATE=utf8mb4_general_ci;
 
--- #3 borrow table 
+-- #3 borrow table
 CREATE TABLE IF NOT EXISTS borrow (
     -- Primary key for the borrow table
-    borrow_id INT AUTO_INCREMENT PRIMARY_KEY,
+    borrow_id INT AUTO_INCREMENT PRIMARY KEY,
 
-    -- Foreign key references
-student_id INT NOT NULL,
-book_id INT NOT NULL,
--- Borrow Timestamp not null by default 
-borrow_date TIMESTAMP NOT NULL
-DEFAULT CURRENT_TIMESTAMP 
--- Borrow return timestamp null by default 
-borrow_return_date TIMESTAMP NULL
-DEFAULT NULL,
+    -- Foreign keys for the borrow table
+    student_id INT NOT NULL,
+    book_id INT NOT NULL,
 
---Borrow table constraints and foreign keys
-CONSTRAINT fk_borrow
-    FOREIGN KEY (student_id)
-    REFERENCES students(students_id)
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT,
+    -- Borrow timestamp
+    borrow_date TIMESTAMP NOT NULL
+        DEFAULT CURRENT_TIMESTAMP,
 
-CONSTRAINT fk_borrow_book
-    FOREIGN KEY (book_id)
-    REFERENCES books(book_id)
-    ON UPDATE CASCADE
-    ON DELETE RESTRICT
+    -- Borrow return timestamp null by default
+    borrow_return_date TIMESTAMP NULL
+        DEFAULT NULL,
 
-)ENGINE=InnoDB
+    -- Borrow table constraints and foreign keys
+    CONSTRAINT fk_borrow_student
+        FOREIGN KEY (student_id)
+        REFERENCES students(student_id)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT,
+
+    CONSTRAINT fk_borrow_book
+        FOREIGN KEY (borrow_id)
+        REFERENCES books(book_id)
+        ON UPDATE CASCADE
+        ON DELETE RESTRICT
+) ENGINE=InnoDB
 DEFAULT CHARSET=utf8mb4
-COLLATE=utf8mb4_general_ci;  
+COLLATE=utf8mb4_general_ci;
 
 -- Insert statement #1: Insert Students
 INSERT INTO students( 
@@ -88,7 +88,7 @@ INSERT INTO books(
 ('MISTAH', 'JAMES BOND', 'ACTION & ADVENTURE'),
 ('ANG PROBINSYANO', 'COCO MARTIN', 'AGENT & DOCTOR');
 
---Insert statement #3: Insert Borrow
+-- Insert statement #3: Insert Borrow
 INSERT INTO borrow(
     student_id,
     book_id
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 
-//Database Connection
+// Database Connection
 $host = 'localhost';
 $db = 'it30b_lab_db';
 $user = 'root';
@@ -14,10 +14,10 @@ $options = [
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
     PDO::ATTR_EMULATE_PREPARES => false,
 ];
+
 try{
-    $pdo = new PDO($dsn,$user,$pass,$options);
-    echo 'connection successful';
-}catch(PDOExeption $e){
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch(PDOEXCEPTION $e){
     die("Database connection failed" . $e->getMessage());
 }
 
@@ -25,43 +25,116 @@ try{
 session_start();
 
 // Determine current section
-$section =$_GET['section'] ??'students';
+$section = $_GET['section'] ?? 'students';
 
-// Determine CRUD Operation
+// Determine CRUD operation
 $action = $_GET['action'] ?? '';
 
-//Fetch Students
-if($section=='students'){
+// Fetch students
+if($section === 'students'){
+    $stmt = $pdo->query("   
+        SELECT *
+        FROM students
+        ORDER BY student_id DESC
+    ");
 
-$stmt = $pdo->query("
-SELECT *
-FROM students
-ORDER BY student_id DESC
-");
-
-$students = $stmt->fetchAll();
+    $students = $stmt->fetchAll();
 }
 
+// Create Student
+if($section==='students' && $action==='create'){
+    if($_SERVER['REQUEST_METHOD']==='POST'){
+        $firstName = trim($_POST['student_first_name'] ?? '');
+        $lastName = trim($_POST['student_last_name'] ?? '');
+        $course = trim($_POST['student_course'] ?? '');
+
+        if($firstName !== '' && $lastName !== '' && $course !== ''){
+            $sql = "
+                INSERT INTO students(
+                    student_first_name,
+                    student_last_name,
+                    student_course
+                )
+                VALUES(?,?,?)
+            ";
+
+            $stmt=$pdo->prepare($sql);
+
+            $stmt->execute([
+                $firstName,
+                $lastName,
+                $course
+            ]); 
+
+            header("Location: index.php?section=students");
+            exit;
+        }
+    }
+}
 
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <meta name="viewport" content="width=, initial-scale=1.0">
+    <title>Library System</title>
 </head>
 <body>
-    <h1>Simple Library System</1>
+    <h1>Simple Library System</h1>
     <nav>
         <a href="index.php?section=students">Students</a>
         <a href="index.php?section=books">Books</a>
         <a href="index.php?section=borrow">Borrow</a>
     </nav>
     <hr>
-    <?php if($section === 'students'): ?>
+    <?php if($section === 'students'):?>
         <h1>Students</h1>
+
+        <p>
+            <a href="index.php?section=students&action=create">
+                Add student
+            </a>
+        </p>
+
+        <?php if($action==='create'): ?>
+            <h2>Create Student</h2>
+
+            <form method="POST">
+                <p>
+                <label>First Name:</label>
+                <br>
+                <input  type="text"
+                        name="student_first_name"
+                        required
+                />
+                </p>
+                <p>
+                <label>Last Name:</label>
+                <br>
+                <input  type="text"
+                        name="student_last_name"
+                        required
+                />
+                </p>
+                <p>
+                <label>Course:</label>
+                <br>
+                <input  type="text"
+                        name="student_course"
+                        required
+                />
+                </p>
+
+                <button type="submit">
+                    Save
+                </button>
+
+                <a href="index.php?section=students">
+                    Cancel
+                </a>
+            </form>
+        <?php else: ?>
         <table>
             <thead>
                 <tr>
@@ -69,46 +142,45 @@ $students = $stmt->fetchAll();
                     <th>First Name</th>
                     <th>Last Name</th>
                     <th>Course</th>
-                    <th>Created at</th>
+                    <th>Create at</th>
                     <th>Actions</th>
                 </tr>
             </thead>
             <tbody>
-            <?php foreach($students as $student): ?>
-                <tr>
-                    <td>
-                        <?=htmlspecialchars($student['student_id']) ?>
-                         </td>
-            <td>
-                        <?=htmlspecialchars($student['student_first_name']) ?>
-                         </td>
-            <td>
-                        <?=htmlspecialchars($student['student_last_name']) ?>
-                         </td>
-            <td>
-                        <?=htmlspecialchars($student['student_course']) ?>
-                         </td>
-            <td>
-                        <?=htmlspecialchars($student['student_created_at']) ?>
-            </td>
-            <td>
-                <a>Edit</a>
-                <a>Delete</a>
-            </td>
-            </tr>
-            <?php endforeach?>
-
+                <?php foreach($students as $student): ?>
+                    <tr>
+                        <td>
+                            <?=htmlspecialchars($student['student_id']) ?>
+                        </td>
+                        <td>
+                            <?=htmlspecialchars($student['student_first_name']) ?>
+                        </td>
+                        <td>
+                            <?=htmlspecialchars($student['student_last_name']) ?>
+                        </td>
+                        <td>
+                            <?=htmlspecialchars($student['student_course']) ?>
+                        </td>
+                        <td>
+                            <?=htmlspecialchars($student['student_created_at']) ?>
+                        </td>
+                        <td>
+                            <a>Edit</a>
+                            <a>Delete</a>
+                        </td>
+                    </tr>
+                    <?php endforeach?>
             </tbody>
-            </table>
-
-
+        </table>
+        <?php endif;?>
     <?php endif; ?>
-    <?php if($section === 'books'): ?>
+
+    <?php if($section === 'books'):?>
         <h1>Books</h1>
     <?php endif; ?>
-    <?php if ($section === 'borrow'): ?>
+
+    <?php if($section === 'borrow'):?>
         <h1>Borrow</h1>
-        <?php endif; ?>
-       
+    <?php endif; ?>
 </body>
 </html>
